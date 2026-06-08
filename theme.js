5d0e4ae7ac6ce08b80eb4124620dc5f13a472f66
jonassimkus <jonassimkus@protonmail.com>
2026-06-08 20:49:25 +0000

    added themes

---
+++ theme.js
@@ -0,0 +1,30 @@
+const theme_button = document.getElementById("theme-button")
+
+if (localStorage.getItem("theme") != null){
+    document.body.classList.toggle(localStorage.getItem("theme")  + '-theme');
+    theme_button.textContent = localStorage.getItem("theme")
+}
+
+
+theme_button.addEventListener("click", async () => {
+    document.body.classList.remove('matcha-theme');
+    document.body.classList.remove('nord-theme');
+    document.body.classList.remove('baddie-theme');
+
+ 
+    if (localStorage.getItem('theme') == "nord"){
+        localStorage.setItem('theme', 'matcha');
+        document.body.classList.toggle('matcha-theme');
+    }
+    else if(localStorage.getItem('theme') == "matcha"){
+        localStorage.setItem('theme', 'baddie');
+        document.body.classList.toggle('baddie-theme');
+    }
+    else{
+        localStorage.setItem('theme', 'nord');
+        document.body.classList.toggle('nord-theme');
+    }
+
+    theme_button.textContent = localStorage.getItem("theme")
+
+});
